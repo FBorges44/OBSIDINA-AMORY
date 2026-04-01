04c2febdeb349b209d14aa3097a4479c0d696358
Junior <139707140+Juniorborgesdapaz@users.noreply.github.com>
2026-04-01 18:54:23 -0300

    teste

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main>
+      <header className="hero">
+        <div> 
+          <h1>Teste de next js </h1>
+        </div>
+
+      </header>
+      <section className="grid-startup">
+        <div className="card">Staturp 1</div>
+        <div className="card">Staturp 1</div>
+        <div className="card">Staturp 1</div>
+        <div className="card">Staturp 1</div>
+        </section>      
+      <style jsx>{
+        }</style>
+    </main>
+  );
+}
